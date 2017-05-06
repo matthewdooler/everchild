ec7a123467e51783de51410d1c06217b0dea850b
MatthewDooler <MatthewDooler@users.noreply.github.com>
2017-05-06 20:18:55 +0100

    Add react-game-kit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Loop, Stage, World, Body } from 'react-game-kit';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          hello world
-        </p>
-      </div>
+        <Loop>
+          <Stage>
+              <World>
+                  // TODO
+              </World>
+          </Stage>
+        </Loop>
     );
   }
 }
